2b79398a0e0f3eb68c61e3817bcdcf1b576a41b9
Sanggeon Yun <ggj06281@kookmin.ac.kr>
2021-03-07 19:11:06 +0900

    feat: re npm init & set up babel & eslint & prettier & webpack

--- client/paperflow-web-v1/src/index.js
+++ client/paperflow-web-v1/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './stores/useRootData';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StoreProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StoreProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ client/paperflow-web-v1/src/stores/app.js
@@ -0,0 +1,11 @@
+// import { observable } from 'mobx';
+
+const createStore = () => {
+  const store = {
+  };
+
+  return store;
+};
+
+const store = createStore();
+export default store;
