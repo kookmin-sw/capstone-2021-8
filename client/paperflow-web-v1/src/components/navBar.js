549d9e3e2e403e8e1f2cdf83cd45c23f85b0e38d
Sang-geon Yun <c0510gy@protonmail.ch>
2021-03-19 12:45:19 +0900

    fix bug

--- client/paperflow-web-v1/src/components/navBar.js
+++ client/paperflow-web-v1/src/components/navBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Navbar,
   Nav,
-  NavDropdown,
   Form,
   FormControl,
   Button,
@@ -15,11 +14,6 @@ const NavBar = () => (
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link href="/">메뉴 아이템</Nav.Link>
-        <NavDropdown title="메뉴" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">로그아웃</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">메뉴 예제</NavDropdown.Item>
-        </NavDropdown>
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
