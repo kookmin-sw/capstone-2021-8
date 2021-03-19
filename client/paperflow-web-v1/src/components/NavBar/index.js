0e454fca50a186dc577060734a7ff60627910b7f
Sang-geon Yun <c0510gy@protonmail.ch>
2021-03-19 13:45:27 +0900

    feat: implement scss

--- client/paperflow-web-v1/src/components/NavBar/index.js
+++ client/paperflow-web-v1/src/components/NavBar/index.js
@@ -7,20 +7,28 @@ import {
   Button,
 } from 'react-bootstrap';
 
-const NavBar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="/">PaperFlow</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/">메뉴 아이템</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Navbar.Collapse>
-  </Navbar>
-);
+import stylesDesktopDefault from './DesktopDefault.module.scss';
+import stylesMobileDefault from './MobileDefault.module.scss';
+
+const NavBar = ({ screenClass }) => {
+  const isDesktop = screenClass === 'xl';
+  const styles = isDesktop ? stylesDesktopDefault : stylesMobileDefault;
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/" className={styles.logo}>PaperFlow</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">메뉴 아이템</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default NavBar;
