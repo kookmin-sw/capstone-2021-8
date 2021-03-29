880a92c26ee55503aedca6849e921b188974904f
Sang-geon Yun <c0510gy@protonmail.ch>
2021-03-29 18:25:30 +0900

    feat: add carousel to landing page

--- client/paperflow-web-v1/src/pages/Landing/index.js
+++ client/paperflow-web-v1/src/pages/Landing/index.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import {
+  Carousel,
+} from 'react-bootstrap';
+
+import backgroundImage from '../../assets/images/concept-1868728_1920';
 
 const App = () => (
   <div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     Landing Page<br />hello, world!
   </div>
 );
