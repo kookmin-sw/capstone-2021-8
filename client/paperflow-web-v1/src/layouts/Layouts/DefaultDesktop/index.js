9bee7664b71d7866594ab3b2a06d2386a0d51b5f
Sang-geon Yun <c0510gy@protonmail.ch>
2021-04-03 16:14:34 +0900

    feat & refactor: add searchbar on Search page & refactor layouts

--- client/paperflow-web-v1/src/layouts/Layouts/DefaultDesktop/index.js
+++ client/paperflow-web-v1/src/layouts/Layouts/DefaultDesktop/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import {
+  Container,
+} from 'react-bootstrap';
 import NavBar from '../../../components/NavBar';
 import Footer from '../../../components/Footer';
 
 const DefaultDesktopLayout = ({ children, className }) => (
   <div className={className}>
     <NavBar />
-    {children}
+    <Container fluid="lg">
+      {children}
+    </Container>
     <Footer />
   </div>
 );
--- client/paperflow-web-v1/src/pages/Search/index.js
+++ client/paperflow-web-v1/src/pages/Search/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Form,
 } from 'react-bootstrap';
 import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
@@ -15,7 +16,17 @@ const Search = () => {
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
 
   return (
-    <DefaultLayout />
+    <DefaultLayout>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Search Papers</Form.Label>
+          <Form.Control type="text" placeholder="논문 이름으로 검색" />
+          <Form.Text className="text-muted">
+            논문 이름으로 검색할 수 있습니다.
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </DefaultLayout>
   );
 };
 
