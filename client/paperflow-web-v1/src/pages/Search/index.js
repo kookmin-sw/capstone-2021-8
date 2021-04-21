028520d392df6ef94f1c320ac25d2630f75788a8
Sang-geon Yun <c0510gy@protonmail.ch>
2021-04-21 23:10:19 +0900

    fix: install babel-polyfill to support async/await

--- client/paperflow-web-v1/src/pages/Search/index.js
+++ client/paperflow-web-v1/src/pages/Search/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Form,
 } from 'react-bootstrap';
+import axios from 'axios';
 import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
 import DefaultLayout from '../../layouts/Layouts/Default';
 import PaperListItem from '../../components/PaperListItem';
+import {
+  parseQueryString,
+} from '../../utils/utility';
+import config from '../../config';
 
 const Search = () => {
   const { screenClass } = useRootData(({ appStore }) => ({
@@ -16,17 +21,32 @@ const Search = () => {
   // eslint-disable-next-line no-unused-vars
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
 
+  const { search } = parseQueryString();
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      setSearchKeyword(search);
+
+      try {
+        await axios.get(`${config.backendEndPoint}/search-paper`, {
+          params: { searchKeyword: search },
+        });
+      } catch {
+        console.log('hi');
+      }
+    }());
+  }, []);
+
   return (
     <DefaultLayout>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Search Papers</Form.Label>
-          <Form.Control type="text" placeholder="논문 이름으로 검색" />
-          <Form.Text className="text-muted">
-            논문 이름으로 검색할 수 있습니다.
-          </Form.Text>
-        </Form.Group>
-      </Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Search Papers</Form.Label>
+        <Form.Control type="text" placeholder="논문 이름으로 검색" value={searchKeyword} />
+        <Form.Text className="text-muted">
+          논문 이름으로 검색할 수 있습니다.
+        </Form.Text>
+      </Form.Group>
       <hr />
       <PaperListItem
         title="Scaling of Magnetic Dissipation and Particle Acceleration in ABC Fields"
--- client/paperflow-web-v1/webpack.config.js
+++ client/paperflow-web-v1/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
