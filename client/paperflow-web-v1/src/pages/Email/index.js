eb920aad1d6038250002bcd1851b02b91922c242
hughugh <lsjppp8069@kookmin.ac.kr>
2021-04-24 21:53:59 +0900

    feat: Add new sentences for email page

--- client/paperflow-web-v1/src/pages/Email/index.js
+++ client/paperflow-web-v1/src/pages/Email/index.js
@@ -9,15 +9,29 @@ const Email = () => {
 
   return (
     <DefaultDesktopLayout>
-      <div className={styles.emailContainer}>
-        <div>
-          만약 지속적으로 연락을 받고 싶다면 이메일을 입력하세요.
-        </div>
-        <div className={styles.emailInputContainer}>
-          <input className={styles.emailInput} />
-          <Button>
-            Submit
-          </Button>
+      <div className={styles.container}>
+        <div className={styles.emailContainer}>
+          <div>
+            <h2>
+              PaperFlow에서는 매달 arXiv에 출판된 논문을
+            </h2>
+            <h2>
+              분석, 통계 및 시각화하여 제공하고 있습니다.
+            </h2>
+            <br />
+            <div>
+              만약 지속적으로 소식을 받고 싶다면 이메일을 입력하세요.
+            </div>
+          </div>
+          <div className={styles.emailInputContainer}>
+            <input
+              className={styles.emailInput}
+              placeholder="email"
+            />
+            <Button>
+              Submit
+            </Button>
+          </div>
         </div>
       </div>
     </DefaultDesktopLayout>
