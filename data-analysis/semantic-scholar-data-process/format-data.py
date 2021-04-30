92198859fc17c376299fecf98322b2cc5fed81fa
Sang-geon Yun <c0510gy@protonmail.ch>
2021-04-30 18:28:01 +0900

    feat: add semantic data formatting code

---
+++ data-analysis/semantic-scholar-data-process/format-data.py
@@ -0,0 +1,35 @@
+import json
+
+PATH = '/Volumes/PSSD/sementicscholar_data/'
+PATH_TO = '/Volumes/PSSD/sementicscholar_data_processed/'
+
+START_IDX = 0
+NUM = 6000
+
+cnt = 0
+# tot = 191281586
+for i in range(START_IDX, NUM):
+    allPapers = []
+
+    filename = PATH + 's2-corpus-{:03d}'.format(i)
+    print(filename)
+    with open(filename, 'r') as f:
+        papers = f.read()
+        f.close()
+
+    paperJson = ''
+    for idx, paper in enumerate(papers.splitlines()):
+        paperJson += paper
+        try:
+            paperObject = json.loads(paperJson)
+            paperJson = ''
+            allPapers.append(paperObject)
+        except:
+            pass
+
+    cnt += len(allPapers)
+
+    with open('{}corpus-{:04d}'.format(PATH_TO, i), 'w') as f:
+        json.dump(allPapers, f)
+
+    print(i, cnt)
