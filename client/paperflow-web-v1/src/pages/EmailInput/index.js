e619556eff013f1ea7aa8df816e7b8bc56360e9c
hughugh <lsjppp8069@kookmin.ac.kr>
2021-04-30 17:08:41 +0900

    fix: Minor fix

--- client/paperflow-web-v1/src/pages/EmailInput/index.js
+++ client/paperflow-web-v1/src/pages/EmailInput/index.js
@@ -1,37 +1,44 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
+import config from '../../config';
+import AlertModal from '../../components/AlertModal';
+import useRootData from '../../hooks/useRootData';
 import DefaultDesktopLayout from '../../layouts/Layouts/DefaultDesktop';
 
 import stylesDesktopDefault from './DesktopDefault.module.scss';
 
 const EmailInputPage = () => {
+  const {
+    changeAlertModalContent,
+  } = useRootData(({ appStore }) => ({
+    changeAlertModalContent: appStore.changeAlertModalContent,
+  }));
+
   const styles = stylesDesktopDefault;
 
   const [email, setEmail] = useState('');
 
   const handleSubmit = () => {
     if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(email)) {
-      axios.post('http://127.0.0.1:8000/newsletter/email/', {
+      axios.post(`${config.backendEndPoint}/newsletter/email/`, {
         email,
       })
         .then(() => {
-          // eslint-disable-next-line no-alert
-          alert('Created successfully');
+          changeAlertModalContent('성공적으로 구독했습니다.');
           setEmail('');
         })
         .catch(() => {
-          // eslint-disable-next-line no-alert
-          alert('There is an error. Try again.');
+          changeAlertModalContent('무언가 잘못됬습니다. 다시 시도해주세요.');
         });
     } else {
-      // eslint-disable-next-line no-alert
-      alert('Not validate email. Check your input.');
+      changeAlertModalContent('유효하지 않은 이메일 주소입니다.');
     }
   };
 
   return (
     <DefaultDesktopLayout>
+      <AlertModal />
       <div className={styles.container}>
         <div className={styles.emailContainer}>
           <div>
