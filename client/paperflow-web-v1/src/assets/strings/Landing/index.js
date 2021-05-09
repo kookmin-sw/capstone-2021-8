60c6c6669176f464750f4df331d7433ee8424c85
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-09 16:17:54 +0900

    feat: implement title of features & short content

--- client/paperflow-web-v1/src/assets/strings/Landing/index.js
+++ client/paperflow-web-v1/src/assets/strings/Landing/index.js
@@ -21,22 +21,22 @@ export const achvs = [
 export const features = [
   {
     image: searchFeatureImage,
-    title: '검색 피쳐',
-    content: 'PaperFlow의 feature 1이 들어갈 위치입니다.',
+    title: '약 1,400만개의 논문을 검색해 보세요',
+    content: 'PaperFlow는 약 1,400만개의 Computer Science분야 논문을 제공하고 있습니다. 이 수많은 논문의 바다에서 원하는 논문을 찾아보세요!',
   },
   {
     image: paperFlowFeatureImage,
-    title: '논문 흐름 피쳐',
-    content: 'PaperFlow의 feature 1이 들어갈 위치입니다.',
+    title: '논문 흐름을 쉽게 파악해 보세요',
+    content: 'PaperFlow에서 제공하는 논문 흐름 기능을 통해 검색한 논문이 과거에 어떤 논문으로 부터 영향을 받았고, 현재 어떤 논문에 영향을 끼치고 있는지 파악할 수 있습니다.',
   },
   {
     image: keywordGraphFeatureImage,
-    title: '키워드 그래프 피쳐',
-    content: 'PaperFlow의 feature 1이 들어갈 위치입니다.',
+    title: '최신 연구 키워드를 통해 최신 연구 경향을 알아보세요',
+    content: 'PaperFlow는 여러분이 쉽게 연구 경향을 파악할 수 있도록 논문의 키워드를 분석하여 그래프로 시각화한 키워드 그래프를 제공합니다.',
   },
   {
     image: subscriptionFeatureImage,
-    title: '구독 피쳐',
-    content: 'PaperFlow의 feature 1이 들어갈 위치입니다.',
+    title: '최신 연구 통계를 매달 받아보세요',
+    content: 'PaperFlow에서는 매달 논문 자료를 분석하여 지난달 연구 통계에 대한 정보를 제공합니다. PaperFlow를 구독하여 최신 연구 흐름을 쉽게 받아보세요!',
   },
 ];
