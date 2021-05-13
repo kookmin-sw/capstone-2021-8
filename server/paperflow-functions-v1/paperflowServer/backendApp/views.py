1c360761099a4664abe2d972a4eb4ecb92c6bb63
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-13 09:47:56 +0900

    feat: add paper view for GET method for a paper

--- server/paperflow-functions-v1/paperflowServer/backendApp/views.py
+++ server/paperflow-functions-v1/paperflowServer/backendApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from paperData.models import PaperInfo
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 
@@ -19,6 +20,37 @@ ES = Elasticsearch(
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+@csrf_exempt
+def paper(request):
+    paperId = request.GET.get('paperId', '')
+
+    try:
+        result = PaperInfo.objects.filter(paper_id=paperId)[0]
+        paperInfo = {
+            "paperId": result.paper_id,
+            "title": result.title,
+            "abstract": result.abstract,
+            "pdf_urls": result.pdf_urls,
+            "authors": result.authors,
+            "citation_list": result.citation_list,
+            "reference_list": result.reference_list,
+            "field_list": result.field_list,
+            "publication_year": result.publication_year,
+            "venue": result.venue,
+            "journal_name": result.journal_name,
+            "journal_volume": result.journal_volume,
+            "journal_pages": result.journal_pages,
+            "doi": result.doi,
+            "mag_id": result.mag_id,
+        }
+        resp = JsonResponse({'paper': paperInfo})
+        resp['Access-Control-Allow-Origin'] = '*'
+
+        return resp
+    except Exception as err:
+        return JsonResponse({'error': str(err)})
 
 
 @csrf_exempt
