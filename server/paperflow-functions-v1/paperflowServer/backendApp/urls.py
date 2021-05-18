42760bd1b6e49f2903595ab75801d1b7650c6e1f
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-18 13:35:49 +0900

    feat: add serializer & getPaperFlow logic

--- server/paperflow-functions-v1/paperflowServer/backendApp/urls.py
+++ server/paperflow-functions-v1/paperflowServer/backendApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('paper', views.paper, name='paper'),
+    path('paper-flow', views.getPaperFlow, name='paper'),
     path('search-paper', views.searchPaper, name='searchPaper'),
     path('', views.index, name='index'),
 ]
