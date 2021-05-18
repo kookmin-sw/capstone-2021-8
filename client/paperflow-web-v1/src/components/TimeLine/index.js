d349ba9456302508f5e570684b3becdfd6fabea7
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-18 14:23:11 +0900

    feat: add TimeLine component

---
+++ client/paperflow-web-v1/src/components/TimeLine/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+const TimeLine = () => (
+  <VerticalTimeline>
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+      date="2011 - present"
+      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    >
+      <h3 className="vertical-timeline-element-title">Creative Director</h3>
+      <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+      <p>
+        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+      </p>
+    </VerticalTimelineElement>
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      date="2010 - 2011"
+      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    >
+      <h3 className="vertical-timeline-element-title">Art Director</h3>
+      <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+      <p>
+        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+      </p>
+    </VerticalTimelineElement>
+  </VerticalTimeline>
+);
+
+export default TimeLine;
