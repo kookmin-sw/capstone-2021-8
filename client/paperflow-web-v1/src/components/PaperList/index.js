3b2ad8e3865bbbcc1f99ba803193e50362fba927
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-18 21:24:22 +0900

    feat: take paper ids instead of paper objects

--- client/paperflow-web-v1/src/components/PaperList/index.js
+++ client/paperflow-web-v1/src/components/PaperList/index.js
@@ -1,42 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
 } from 'react-bootstrap';
 import PaperListItem from '../PaperListItem';
+import { fetchPaper } from '../../utils/utility';
+import BlankListItem from '../BlankListItem';
 
 const PaperList = ({
-  papers,
+  paperIds,
 }) => {
   const pageSize = 10;
   const [currentPageSize, setCurrentPageSize] = useState(pageSize);
+  const [fetchedPapers, setFetchedPapers] = useState([]);
+
+  const fetchPageSizePapers = async () => {
+    setFetchedPapers([
+      ...fetchedPapers,
+      ...await Promise.all(
+        paperIds.slice(currentPageSize - pageSize, currentPageSize)
+          .map((paperId) => fetchPaper(paperId)),
+      ),
+    ]);
+  };
+
+  useEffect(() => {
+    fetchPageSizePapers();
+  }, [currentPageSize]);
 
   return (
     <div style={{ marginBottom: '20px' }}>
       {
-        papers.slice(0, currentPageSize).map(({
-          paperId, title, date, authors, abstract, highlightKeywords, keywords,
-        }) => (
-          <div
-            key={paperId}
-            style={{ marginBottom: '10px' }}
-          >
-            <PaperListItem
-              paperId={paperId}
-              title={title}
-              date={date}
-              authors={authors}
-              abstract={abstract}
-              highlightKeywords={highlightKeywords}
-              keywords={keywords}
-              compact
-            />
-          </div>
-        ))
+        fetchedPapers && fetchedPapers.map((paper) => {
+          if (!paper) {
+            return (
+              <div
+                style={{ marginBottom: '10px' }}
+              >
+                <BlankListItem content="Computer Science 분야의 논문이 아닙니다!" />
+              </div>
+            );
+          }
+
+          const {
+            paperId, title, date, authors, abstract, highlightKeywords, keywords,
+          } = paper;
+
+          return (
+            <div
+              key={paperId}
+              style={{ marginBottom: '10px' }}
+            >
+              <PaperListItem
+                paperId={paperId}
+                title={title}
+                date={date}
+                authors={authors.map((author) => author.name)}
+                abstract={abstract}
+                highlightKeywords={highlightKeywords}
+                keywords={keywords}
+                compact
+              />
+            </div>
+          );
+        })
       }
       {
-        papers.length > currentPageSize && (
-          <Button variant="outline-primary" size="md" onClick={() => setCurrentPageSize(currentPageSize + pageSize)} block>
-            더보기{` (${Math.min(currentPageSize, papers.length)}/${papers.length})`}
+        paperIds.length > currentPageSize && (
+          <Button
+            variant="outline-primary"
+            size="md"
+            onClick={() => {
+              setCurrentPageSize(currentPageSize + pageSize);
+            }}
+            block
+          >
+            더보기{` (${Math.min(currentPageSize, paperIds.length)}/${paperIds.length})`}
           </Button>
         )
       }
