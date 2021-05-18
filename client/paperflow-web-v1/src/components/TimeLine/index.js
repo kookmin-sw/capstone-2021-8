bc5ae87ee152f3c5686683461ccc40015def0df4
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-18 17:03:15 +0900

    feat: implement custom line color

--- client/paperflow-web-v1/src/components/TimeLine/index.js
+++ client/paperflow-web-v1/src/components/TimeLine/index.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import useRootData from '../../hooks/useRootData';
 import 'react-vertical-timeline-component/style.min.css';
+import './customVerticalTimeLine.css';
 
-const TimeLine = () => (
-  <VerticalTimeline>
-    <VerticalTimelineElement
-      className="vertical-timeline-element--work"
-      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-      date="2011 - present"
-      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    >
-      <h3 className="vertical-timeline-element-title">Creative Director</h3>
-      <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-      <p>
-        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-      </p>
-    </VerticalTimelineElement>
-    <VerticalTimelineElement
-      className="vertical-timeline-element--work"
-      date="2010 - 2011"
-      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    >
-      <h3 className="vertical-timeline-element-title">Art Director</h3>
-      <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-      <p>
-        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-      </p>
-    </VerticalTimelineElement>
-  </VerticalTimeline>
-);
+const TimeLine = () => {
+  const { screenClass } = useRootData(({ appStore }) => ({
+    screenClass: appStore.screenClass.get(),
+  }));
+  // eslint-disable-next-line no-unused-vars
+  const isDesktop = screenClass === 'xl';
+
+  // const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
+  return (
+    <VerticalTimeline className="vertical-timeline-custom-line">
+      <VerticalTimelineElement
+        className="vertical-timeline-element"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+      >
+        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element"
+        date="2010 - 2011"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+      >
+        <h3 className="vertical-timeline-element-title">Art Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+        </p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+  );
+};
 
 export default TimeLine;
