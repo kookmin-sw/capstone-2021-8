48ecfcdcb0ebd12ecc076561c09ea8a1a277aedb
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-20 18:56:48 +0900

    feat: make 100% height on layout

--- client/paperflow-web-v1/src/layouts/Layouts/FullWidthNoFooter/index.js
+++ client/paperflow-web-v1/src/layouts/Layouts/FullWidthNoFooter/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import NavBar from '../../../components/NavBar';
+import stylesDefault from './Default.module.scss';
+import './DefaultStyles.css';
 
-const FullWidthNoFooterLayout = ({ children, className }) => (
-  <div className={className}>
-    <NavBar />
-    {children}
-  </div>
-);
+const FullWidthNoFooterLayout = ({ children, className }) => {
+  const styles = stylesDefault;
+
+  return (
+    <div className={`${styles.container} ${className || ''}`}>
+      <NavBar />
+      {children}
+    </div>
+  );
+};
 
 export default FullWidthNoFooterLayout;
